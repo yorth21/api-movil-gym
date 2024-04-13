0474d6f965a2a19b75f37c40e7c605f3bf70e633
Yorth Ortegon <yorth21@gmail.com>
2024-04-13 18:22:41 -0500

    Add createUser method to UserController

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,4 +9,13 @@ export class UserController {
       res.status(500).json({ message: error.message })
     }
   }
+
+  static async createUser (req, res) {
+    try {
+      const newUser = await UserRepository.createUser(req.body)
+      res.status(201).json(newUser)
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+  }
 }
