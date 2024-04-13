d17f064705cd68f21289bb56846dd3d594cf5f42
Yorth Ortegon <yorth21@gmail.com>
2024-04-13 15:02:40 -0500

    Refactor the new implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import app from './app.js'
 import http from 'http'
 import 'dotenv/config'
-import { sequelize } from './database/database.js'
+import { sequelize } from './data/postgres/connection.js'
 
-import './models/User.js'
+import './data/postgres/models/user.model.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -11,7 +11,8 @@ const server = http.createServer(app)
 
 server.listen(PORT, async () => {
   try {
-    await sequelize.sync()
+    // await sequelize.sync({ force: false })
+    await sequelize.authenticate()
     console.log(`Server escuchando en el puerto: ${PORT}`)
   } catch (error) {
     console.error('No se pudo conectar con la base de datos:', error)
