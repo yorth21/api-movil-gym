e95c323e6fc4c6a28e6b45941533b5e3bf68c4ad
Yorth Ortegon <yorth21@gmail.com>
2024-04-14 18:01:11 -0500

    Add login route to auth.routes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { usersRoutes } from './user.routes.js'
+import { authRoutes } from './auth.routes.js'
 
 export const Routes = Router()
 
 Routes
+  .use('/auth', authRoutes)
   .use('/users', usersRoutes)
