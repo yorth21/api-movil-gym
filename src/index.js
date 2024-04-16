861bc1c0e243ac22e32a4dfb926b80ee86d8d638
Yorth Ortegon <yorth21@gmail.com>
2024-04-15 22:47:04 -0500

    Add new models for physical information and registration diary entries

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { sequelize } from './data/postgres/connection.js'
 import app from './app.js'
 
 import './data/postgres/models/user.model.js'
+import './data/postgres/models/physicalInfo.model.js'
+import './data/postgres/models/regDiary.model.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -11,7 +13,7 @@ const server = http.createServer(app)
 
 server.listen(PORT, async () => {
   try {
-    await sequelize.sync({ force: false })
+    await sequelize.sync({ force: true })
     console.log(`Server escuchando en el puerto: ${PORT}`)
   } catch (error) {
     console.error('No se pudo conectar con la base de datos:', error)
