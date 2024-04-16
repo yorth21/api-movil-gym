d731a29498f7b1971d25a822d1b8dd85ff05472a
Yorth Ortegon <yorth21@gmail.com>
2024-04-16 00:25:05 -0500

    Refactor PhysicalInfoRepository to use async/await syntax

---
+++ src/data/postgres/repositories/physicalInfo.repository.js
@@ -0,0 +1,57 @@
+import { PhysicalInfoModel } from '../models/physicalInfo.model.js'
+
+export class PhysicalInfoRepository {
+  static async getPhysicalInfos () {
+    try {
+      const physicalInfos = await PhysicalInfoModel.findAll()
+      return physicalInfos
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  static async createPhysicalInfo (physicalInfo) {
+    try {
+      const newPhysicalInfo = await PhysicalInfoModel.create(physicalInfo)
+      return newPhysicalInfo
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  static async updatePhysicalInfo (id, physicalInfo) {
+    try {
+      const physicalInfoToUpdate = await PhysicalInfoModel.findByPk(id)
+      if (!physicalInfoToUpdate) {
+        return null
+      }
+      await physicalInfoToUpdate.update(physicalInfo)
+      return physicalInfoToUpdate
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  static async deletePhysicalInfo (id) {
+    try {
+      const physicalInfoToDelete = await PhysicalInfoModel.findByPk(id)
+      if (!physicalInfoToDelete) {
+        return null
+      }
+      physicalInfoToDelete.isActive = false
+      await physicalInfoToDelete.save()
+      return physicalInfoToDelete
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  static async getPhysicalInfoByUserId (userId) {
+    try {
+      const physicalInfo = await PhysicalInfoModel.findOne({ where: { userId } })
+      return physicalInfo
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+}
