3934b299c63de5bd8f7361f607100b51e310814a
Yorth Ortegon <yorth21@gmail.com>
2024-04-15 22:46:32 -0500

    Add PhysicalInfoModel to handle physical information in the database

---
+++ src/data/postgres/models/physicalInfo.model.js
@@ -0,0 +1,48 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../connection.js'
+
+export const PhysicalInfoModel = sequelize.define('physical_info', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  height: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  weight: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  weightGoal: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    field: 'weight_goal'
+  },
+  physicalGoal: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'physical_goal'
+  },
+  physicalLevel: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'physical_level'
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+    field: 'is_active'
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at'
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'updated_at'
+  }
+})
