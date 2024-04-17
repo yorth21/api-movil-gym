41989c9003ff6eea8608eb9f9c32d922d93acae3
Yorth Ortegon <yorth21@gmail.com>
2024-04-16 19:33:35 -0500

    Add routes catExcercise and excercise

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import { usersRoutes } from './user.routes.js'
 import { authRoutes } from './auth.routes.js'
+import { catExcerciseRoutes } from './catExcercise.routes.js'
+import { excerciseRoutes } from './excercise.routes.js'
 
 export const Routes = Router()
 
 Routes
   .use('/auth', authRoutes)
   .use('/users', usersRoutes)
+  .use('/catExcercise', catExcerciseRoutes)
+  .use('/excercise', excerciseRoutes)
