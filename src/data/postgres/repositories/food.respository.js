2b36f64f64079868702c7d25c88bcd6e8cf1ed51
Yorth Ortegon <yorth21@gmail.com>
2024-04-22 18:24:48 -0500

    Update FoodRepository to include IngredientModel in getFoods method

--- src/data/postgres/repositories/food.respository.js
+++ src/data/postgres/repositories/food.respository.js
@@ -1,9 +1,15 @@
 import { FoodModel } from '../models/food.model.js'
+import { IngredientModel } from '../models/ingredient.model.js'
 
 export class FoodRepository {
   static async getFoods () {
     try {
-      const foods = await FoodModel.findAll()
+      const foods = await FoodModel.findAll({
+        include: {
+          model: IngredientModel,
+          attributes: ['name']
+        }
+      })
       return foods
     } catch (error) {
       throw new Error(error)
